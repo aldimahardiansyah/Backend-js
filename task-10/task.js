fffaeb7dc029b1f71a22b12d9b27191f359f7bcf
aldimahardiansyah <aldimdiansyah@gmail.com>
2021-12-10 12:51:11 +0700

    task belum beres

---
+++ task-10/task.js
@@ -0,0 +1,30 @@
+/**
+ * Fungsi untuk menampilkan hasil download
+ * @param {string} result - Nama file yang didownload
+ */
+ function showDownload(result) {
+    console.log("Download selesai");
+    console.log("Hasil Download: " + result);
+  }
+  
+  /**
+   * Fungsi untuk download file
+   * @param {function} callback - Function callback show
+   */
+  function download(callShowDownload) {
+    return new Promise(function(resolve, reject){
+        setTimeout(function () {
+            const result = "windows-10.exe";
+            resolve(callShowDownload(result));
+          }, 3000);
+    })
+}
+  
+  download(showDownload)
+  
+  /**
+   * TODO:
+   * - Refactor callback ke Promise atau Async Await
+   * - Refactor function ke ES6 Arrow Function
+   * - Refactor string ke ES6 Template Literals
+   */
